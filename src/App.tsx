35f7c0ebc7ce41c13ffbe7134c3ddb7da06b44ed
Artie Gomes <luizarthursjp@gmail.com>
2025-12-12 10:23:07 -0300

    refactor: navbar.tsx is now in its own subfolder inside components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import Home from "./pages/home/Home";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 
 export function App() {
   return (
