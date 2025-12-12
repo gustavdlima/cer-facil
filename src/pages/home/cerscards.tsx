da348e97c40dd532a7b85a8e3dde4cd7223201f5
amandafsm <massilvaa21.com>
2025-12-11 21:27:42 -0300

    feat:add cers-cards to scr

---
+++ src/pages/home/cerscards.tsx
@@ -0,0 +1,8 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function NetworkInfo() {
+    return (
+        
+    )
+}
