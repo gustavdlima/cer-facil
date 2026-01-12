fadb07c2e4b160aa1ba9bad8416fa2414bf99ceb
amandafsm <massilvaa21.com>
2026-01-12 17:10:04 -0300

    feat: add cer map cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./index.css";
 import Home from "./pages/home/Home";
 import { Navbar } from "./components/Navbar/Navbar";
+import PbMap from "./components/pb-map/pb-map";
 
 export function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <PbMap />
     </>
   );
 }
