8f5e7dccf379b424652699faec6c4bcd4405a178
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2026-01-13 13:23:03 -0300

    build: refactoring dir and components

---
+++ src/pages/home/components/welcome-page/Welcome.tsx
@@ -0,0 +1,26 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function Welcome() {
+    return (
+        <div>
+             <Card className="max-w-2xl mx-auto">
+                <CardHeader>
+                <CardTitle>Bem-vindo ao CER Fácil</CardTitle>
+                <CardDescription>
+                </CardDescription>
+                </CardHeader>
+                <CardContent>
+                <p className="text-muted-foreground mb-4">
+                    Este mapa inteligente foi criado para conectar pessoas com deficiência e suas famílares aos Centros Especializados em Reabilitação (CER's) do estado. De forma rápida e personalizada, você poderá:
+
+                    Entender o que são os CER's e como funciona a rede de cuidado.
+
+                    Descobrir quais unidades atendem ao tipo de deficiência que você busca.
+
+                    Encontrar o CER mais adequado com base no seu município e necessidades específicas.
+                </p>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
