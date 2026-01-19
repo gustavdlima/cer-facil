69407532f4d16519cec190bc9bd9b19cfe678f56
amandafsm <massilvaa21.com>
2026-01-19 10:58:39 -0300

    style(map): make legend visual more discreet

--- src/components/pb-map/mapcaptions.tsx
+++ src/components/pb-map/mapcaptions.tsx
@@ -4,25 +4,25 @@ import { CORES_REGIOES } from './macros/data';
 function MapCaptions() {
   return (
     <div className='flex justify-end w-full pr-10 pb-10 pr-15'>
-      <div className='grid grid-cols-3 gap-x-8 gap-y-2 w-fit'>
+      <div className='grid grid-cols-3 gap-x-6 gap-y-1 w-fit'>
         {Object.entries(MACROS_PB).flatMap(([macroId, macroObj]) =>
           Object.entries(macroObj).map(([regiaoId, regiaoObj]) => (
             <div 
               key={`${macroId}-${regiaoId}`} 
-              className='flex items-center justify-end gap-2 scale-90 origin-right'
+              className='flex items-center justify-end gap-2'
             >
               <span
                 style={{
                   fontSize: '14px',
                   color: 'black',
-                  fontWeight: 'bold',
+                  //fontWeight: 'bold',
                   whiteSpace: 'nowrap'
                 }}
               >
                 {regiaoObj.nome}
               </span>
               <div
-                className='w-3 h-3 rounded-sm flex-shrink-0'
+                className='w-2.5 h-2.5 rounded-[2px] flex-shrink-0 opacity-90'
                 style={{ backgroundColor: CORES_REGIOES[parseInt(regiaoId) - 1] }}
               />
             </div>
