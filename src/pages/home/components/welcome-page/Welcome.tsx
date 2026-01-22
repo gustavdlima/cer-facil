381e05bb2dbc6a6a6d44f5e8aa1245b1affd63e3
beatrizzzzz <54537982+beatrizzzzz@users.noreply.github.com>
2026-01-21 23:24:53 -0300

    Update Welcome.tsx
    adicionei logo

--- src/pages/home/components/welcome-page/Welcome.tsx
+++ src/pages/home/components/welcome-page/Welcome.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import CerForm from "../form/CerForm";
+import pet from "@/assets/images/pet.jpg";
 
 import {
   Card,
@@ -22,15 +23,30 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
   return (
     <div className="w-screen h-screen flex items-center justify-center p-8">
       <Card className="max-w-2xl">
-        <CardHeader className="text-center">
-          <CardTitle className="text-4xl font-bold">CER Fácil</CardTitle>
-          <CardDescription className="text-lg">
-            Conectamos pessoas com deficiência aos Centros de Reabilitação
-          </CardDescription>
+        
+        <CardHeader>
+          <div className="flex items-center gap-6">
+            {/* Imagem */}
+            <img
+              src={pet}
+              alt="logo pet"
+              className="w-52 h-52 rounded-full object-cover flex-shrink-0"
+            />
+
+            
+            <div>
+              <CardTitle className="text-4xl font-bold">
+                CER Fácil
+              </CardTitle>
+              <CardDescription className="text-lg">
+                Conectamos pessoas com deficiência aos Centros de Reabilitação
+              </CardDescription>
+            </div>
+          </div>
         </CardHeader>
 
         <CardContent>
-          <p className="text-muted-foreground text-center leading-relaxed">
+          <p className="text-muted-foreground leading-relaxed">
             Seja bem-vindo ao CER Fácil. Aplicação voltada a facilitar o
             conhecimento e acesso aos Centros de Reabilitação (CERs) para
             pessoas com deficiência.
@@ -38,7 +54,9 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
         </CardContent>
 
         <CardContent className="text-center">
-          <Button onClick={() => setShowForm(true)}>Começar busca</Button>
+          <Button onClick={() => setShowForm(true)}>
+            Começar busca
+          </Button>
         </CardContent>
       </Card>
     </div>
