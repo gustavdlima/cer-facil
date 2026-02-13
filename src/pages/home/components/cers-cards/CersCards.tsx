e73a4c78d420e4201c9bb45735e3a3e6e8f9bef7
amandafsm <massilvaa21.com>
2026-02-13 10:04:47 -0300

    fix/cer cards adjustments

--- src/pages/home/components/cers-cards/CersCards.tsx
+++ src/pages/home/components/cers-cards/CersCards.tsx
@@ -2,10 +2,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import CERS from "@/data/cers.json";
 import Flow from "../user-flow/Flow";
-import imagemFunad from "@/assets/images/funad.jpeg";
-import imagemInstitutoDosCegos from "@/assets/images/institutoDosCegos.jpeg";
-import imagemCentroDeReabilitacaoAntonioDeSouza from "@/assets/images/centroReabilitacaoAntonioSouza.jpeg";
-import imagemComplexoNeurofuncionalMariaMoura from "@/assets/images/complexoNeurofuncionalMariaMoura.jpeg";
 
 interface CersCardsProps {
   showFlow: boolean;
@@ -26,7 +22,7 @@ export default function CersCards({ showFlow, setShowFlow }: CersCardsProps) {
       className="py-16 bg-[var(--cor-5)] w-screen relative left-[calc(-50vw+50%)] px-6"
     >
 
-      <div className="mx-auto max-w-[90%]">
+      <div className="mx-auto max-w-[100%]">
         <div className="flex items-center w-full mb-12">
           <div className="w-[40%] md:w-[35%] h-[2px] bg-[var(--cor-4)] mr-6"></div>
           <h2
@@ -44,41 +40,19 @@ export default function CersCards({ showFlow, setShowFlow }: CersCardsProps) {
                       md:grid-cols-1    
                       lg:grid-cols-2    
                       xl:grid-cols-4
-                      gap-6             
+                      gap-4  
+                      space-y-4     
+                      space-x-4    
                       justify-items-center
                     "
         >
 
           {(CERS as DadosCers[]).map((cer, index) => (
             <Card key={cer.id} id={`${cer.id}`}
-              style={{ backgroundColor: `var(--cor-${(index % 3) + 1})` } as React.CSSProperties}
-              className="relative max-w-2xl mx-auto w-90 h-150 scroll-mt-20 text-white border-none rounded-none itens-center py-0">
-              {index === 0 && (
-                <img
-                  src={imagemFunad}
-                  alt="funad"
-                  className="w-100 h-70" />
-              )}
-              {index === 1 && (
-                <img
-                  src={imagemInstitutoDosCegos}
-                  alt="instituto dos cegos"
-                  className="w-100 h-70" />
-              )}
-              {index === 2 && (
-                <img
-                  src={imagemCentroDeReabilitacaoAntonioDeSouza}
-                  alt="centro de reabilitação antônio de souza"
-                  className="w-100 h-70" />
-              )}
-              {index === 3 && (
-                <img
-                  src={imagemComplexoNeurofuncionalMariaMoura}
-                  alt="complexo neurofuncional maria moura"
-                  className="w-100 h-70" />
-              )}
-
-
+              style={{ backgroundColor: `var(--cor-1)` } as React.CSSProperties}
+              className="relative max-w-2xl mx-auto w-90 h-80 scroll-mt-20 text-white border-none itens-center py-0">
+              
+            
               <CardHeader>
                 <CardTitle className="mt-10 text-xl text-left font-bold">
                   {cer.nome}
@@ -95,9 +69,7 @@ export default function CersCards({ showFlow, setShowFlow }: CersCardsProps) {
                     </span>
                   </p>
                   <div className="flex justify-end">
-                    <Button className="absolute bottom-5 bg-transparent hover:bg-[var(--cor-4)]" onClick={() => setShowFlow([true, cer.id])}>
-                      <img src="https://www.svgrepo.com/show/425982/right-arrow.svg" alt="" className="w-5 h-5" />
-                    </Button>
+                   <Button className="absolute bottom-5" onClick={() => setShowFlow([true, cer.id])}>Detalhes</Button>
                   </div>
                 </div>
               </CardContent>
