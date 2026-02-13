e8bc1aa3f027258686480a549454ca275a8383a7
Artie Gomes <luizarthursjp@gmail.com>
2026-02-12 22:24:48 -0300

    refactor: now navbar always shows

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
 
   return (
     <>
-      {!showForm && !showFlow[0] && <Navbar />}
+      {!showForm && <Navbar />}
       <Home
         showForm={showForm}
         setShowForm={setShowForm}
