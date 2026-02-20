2060c51e0baf4e7d9340446a2ffb8d2f92329beb
amandafsm <massilvaa21@gmail.com>
2026-02-20 11:32:48 -0300

    fix:adjust footnote

--- src/pages/home/components/Home.tsx
+++ src/pages/home/components/Home.tsx
@@ -30,7 +30,6 @@ export default function Home({
         <>
           <CersCards showFlow={showFlow} setShowFlow={setShowFlow} />
           <NetworkInfo />
-          <Footnote />
         </>
       )}
     </div>
