a6ab08d60bcf069d034d32299e99458497706234
amandafsm <massilvaa21.com>
2026-03-23 18:38:28 -0300

    feat: add logo and reorganize central text layout

--- src/pages/home/components/welcome-page/Welcome.tsx
+++ src/pages/home/components/welcome-page/Welcome.tsx
@@ -1,6 +1,6 @@
 import CerForm from "../form/CerForm";
 import { ChevronDown, MapPin, Network, BookOpen } from "lucide-react";
-
+import logoSeuCer from "../../../../assets/welcome-images/seu_cer_logo.png";
 interface WelcomeProps {
   showForm: boolean;
   setShowForm: (show: boolean) => void;
@@ -26,27 +26,32 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
   }
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center pt-16 pb-32 relative bg-white">
-      <section
-        className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center mb-16"
-      >
-        <h1
-          className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-black leading-tight"
-        >
-          Aproximamos você aos{" "}
-          <span className="text-[var(--cor-bg-1)]">
-            Centros Especializados em Reabilitação (CER)
-          </span>
-          <span> da Paraíba.</span>
-          <br />
-          <span className="block mt-6 text-3xl md:text-4xl font-semibold text-gray-800">
+    <main className="min-h-screen flex flex-col items-center justify-center pt-10 pb-20 relative bg-white">
+      <section className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+
+        <div className="flex justify-center mb-6">
+          <img
+            src={logoSeuCer.src || logoSeuCer}
+            alt="Logo Seu CER"
+            className="h-60 md:h-70 object-contain"
+          />
+        </div>
+
+        
+          <h1 className="text-lg md:text-xl lg:text-2xl font-extrabold text-black leading-tight">
+            Aproximamos você aos{" "}
+            <span className="text-[var(--cor-bg-1)]">
+              Centros Especializados em Reabilitação (CER)
+            </span>
+            <span> da Paraíba.</span>
+          </h1>
+
+
+          <p className="mt-4 text-xl md:text-2xl font-semibold text-gray-700 mb-16">
             Profissionais da Saúde, Pessoas com Deficiência e Familiares.
-          </span>
-          <span className="block mt-6 text-3xl md:text-4xl font-semibold text-gray-800">
-            É um prazer ter você aqui no Seu CER.
-          </span>
-        </h1>
-        <nav aria-label="menu principal de ações" className="w-full max-w-6xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-6 mt-16">
+          </p>
+
+        <nav aria-label="menu principal de ações" className="w-full max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
           <button
             onClick={() => setShowForm(true)}
             className="focus-within:border-10 focus-within:border-[var(--cor-destaque)] flex flex-col items-center p-8 bg-white border-2 border-gray-100 rounded-2xl shadow-sm hover:shadow-xl hover:border-[var(--cor-bg-1)] transition-all cursor-pointer group text-center"
@@ -54,10 +59,11 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
             <div className="p-4 bg-[var(--cor-bg-1)]/10 rounded-full mb-4 group-hover:bg-[var(--cor-bg-1)] transition-colors">
               <MapPin className="w-12 h-12 text-[var(--cor-bg-1)] group-hover:text-white" />
             </div>
-            <span className="text-3xl font-bold text-gray-900">
+            <span className="text-2xl font-bold text-gray-900 leading-snug">
               Busque o CER Mais Próximo
             </span>
           </button>
+
           <button
             onClick={handleScrollToSection}
             className="focus-within:border-10 focus-within:border-[var(--cor-destaque)] flex flex-col items-center p-8 bg-white border-2 border-gray-100 rounded-2xl shadow-sm hover:shadow-xl hover:border-[var(--cor-bg-1)] transition-all cursor-pointer group text-center"
@@ -65,10 +71,11 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
             <div className="p-4 bg-[var(--cor-bg-1)]/10 rounded-full mb-4 group-hover:bg-[var(--cor-bg-1)] transition-colors">
               <Network className="w-12 h-12 text-[var(--cor-bg-1)] group-hover:text-white" />
             </div>
-            <span className="text-3xl font-bold text-gray-900">
+            <span className="text-2xl font-bold text-gray-900 leading-snug">
               Conheça a Rede CER de Cuidados
             </span>
-          </button>{" "}
+          </button>
+
           <button
             onClick={handleScrollToEducationalMaterial}
             className="focus-within:border-10 focus-within:border-[var(--cor-destaque)] flex flex-col items-center p-8 bg-white border-2 border-gray-100 rounded-2xl shadow-sm hover:shadow-xl hover:border-[var(--cor-bg-1)] transition-all cursor-pointer group text-center"
@@ -76,11 +83,11 @@ export default function Welcome({ showForm, setShowForm }: WelcomeProps) {
             <div className="p-4 bg-[var(--cor-bg-1)]/10 rounded-full mb-4 group-hover:bg-[var(--cor-bg-1)] transition-colors">
               <BookOpen className="w-12 h-12 text-[var(--cor-bg-1)] group-hover:text-white" />
             </div>
-            <span className="text-3xl font-bold text-gray-900">
+            <span className="text-2xl font-bold text-gray-900 leading-snug">
               Tenha Acesso a Materiais Educativos
             </span>
-          </button>{" "}
-        </nav>{" "}
+          </button>
+        </nav>
       </section>
     </main>
   );
